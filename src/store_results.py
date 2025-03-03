b64673c8336fb12b2f7ad24ffc0c80fa7a5ea103
Jesus Cogua <jes.cogm@gmail.com>
2025-03-02 22:25:44 -0500

    added solution for saving result in database

--- src/store_results.py
+++ src/store_results.py
@@ -24,7 +24,9 @@ def store_results_in_db(table_name, data, columns):
                 metadata.create_all(engine)
             
             # Insert the new data
-            insert_query = f'INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({', '.join([f":{col}" for col in columns])})'
+            columns_str = ', '.join(columns)
+            values_str = ', '.join([f':{col}' for col in columns])
+            insert_query = f'INSERT INTO {table_name} ({columns_str}) VALUES ({values_str})'
             session.execute(insert_query, data)
             session.commit()
         except SQLAlchemyError as e:
